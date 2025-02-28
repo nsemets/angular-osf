21c8aa2cdb7c5cee1793741c563adac940363da3
Roman Nastyuk <cosmicroman42@gmail.com>
2025-02-28 11:46:59 +0200

    added store to the providers array

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideStore } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideStore([], withNgxsReduxDevtoolsPlugin({ disabled: false })),
   ],
 };
