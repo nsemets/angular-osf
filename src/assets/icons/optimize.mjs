42cb35614a037974e13f72aff26d97337ec6ea95
andrii.siuta <andriisiuta@gmail.com>
2025-03-10 14:17:40 +0200

    feat(chore): add icon generator and optimizer

---
+++ src/assets/icons/optimize.mjs
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const sourceDirectory = './source';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const removeFillAttribute = (svgString) => {
+  return svgString.replace(/ fill="[^"]*"/g, '');
+};
+
+fs.promises
+  .readdir(path.join(__dirname, sourceDirectory))
+  .then((files) => {
+
+    files.forEach((file) => {
+      if (file.endsWith('.svg')) {
+        console.log(file);
+        const filePath = path.join(__dirname, sourceDirectory, file);
+        const command = `picosvg ${filePath}`;
+
+        exec(command, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`Error executing picosvg for ${filePath}: ${error.message}`);
+            return;
+          }
+          const modifiedSVG = removeFillAttribute(stdout);
+          fs.writeFileSync(filePath, modifiedSVG);
+        });
+      }
+    });
+  })
+  .finally(() => {
+    console.log('Optimise Done');
+  })
+  .catch((err) => {
+    console.error(`Error reading directory: ${err}`);
+  });
