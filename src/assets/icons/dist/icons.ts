bee33865c6827a7db74c9b41aeccb87d35a45c88
andrii.siuta <siuta.andrii@gmail.com>
2025-03-10 15:14:21 +0200

    fix(chore): add generated font

---
+++ src/assets/icons/dist/icons.ts
@@ -0,0 +1,225 @@
+export type IconsId =
+  | 'x'
+  | 'website'
+  | 'warning'
+  | 'upload'
+  | 'trash'
+  | 'support'
+  | 'supplements'
+  | 'sort-3'
+  | 'sort-2'
+  | 'sort-1'
+  | 'settings'
+  | 'search'
+  | 'quotes'
+  | 'question-mark'
+  | 'publish'
+  | 'profile'
+  | 'plus'
+  | 'pdf'
+  | 'papers'
+  | 'padlock'
+  | 'padlock-unlock'
+  | 'orchid'
+  | 'my-projects'
+  | 'minus'
+  | 'meetings'
+  | 'materials'
+  | 'list'
+  | 'linkedin'
+  | 'institutions'
+  | 'information'
+  | 'image'
+  | 'hosting'
+  | 'home'
+  | 'group'
+  | 'github'
+  | 'folder'
+  | 'filter'
+  | 'facebook'
+  | 'eye-view'
+  | 'eye-hidden'
+  | 'email'
+  | 'download'
+  | 'double-arrow-left'
+  | 'dots'
+  | 'donate'
+  | 'doc'
+  | 'design'
+  | 'data'
+  | 'copy'
+  | 'collections'
+  | 'collect'
+  | 'code'
+  | 'calendar-silhouette'
+  | 'arrow-down';
+
+export type IconsKey =
+  | 'X'
+  | 'Website'
+  | 'Warning'
+  | 'Upload'
+  | 'Trash'
+  | 'Support'
+  | 'Supplements'
+  | 'Sort3'
+  | 'Sort2'
+  | 'Sort1'
+  | 'Settings'
+  | 'Search'
+  | 'Quotes'
+  | 'QuestionMark'
+  | 'Publish'
+  | 'Profile'
+  | 'Plus'
+  | 'Pdf'
+  | 'Papers'
+  | 'Padlock'
+  | 'PadlockUnlock'
+  | 'Orchid'
+  | 'MyProjects'
+  | 'Minus'
+  | 'Meetings'
+  | 'Materials'
+  | 'List'
+  | 'Linkedin'
+  | 'Institutions'
+  | 'Information'
+  | 'Image'
+  | 'Hosting'
+  | 'Home'
+  | 'Group'
+  | 'Github'
+  | 'Folder'
+  | 'Filter'
+  | 'Facebook'
+  | 'EyeView'
+  | 'EyeHidden'
+  | 'Email'
+  | 'Download'
+  | 'DoubleArrowLeft'
+  | 'Dots'
+  | 'Donate'
+  | 'Doc'
+  | 'Design'
+  | 'Data'
+  | 'Copy'
+  | 'Collections'
+  | 'Collect'
+  | 'Code'
+  | 'CalendarSilhouette'
+  | 'ArrowDown';
+
+export enum Icons {
+  X = 'x',
+  Website = 'website',
+  Warning = 'warning',
+  Upload = 'upload',
+  Trash = 'trash',
+  Support = 'support',
+  Supplements = 'supplements',
+  Sort3 = 'sort-3',
+  Sort2 = 'sort-2',
+  Sort1 = 'sort-1',
+  Settings = 'settings',
+  Search = 'search',
+  Quotes = 'quotes',
+  QuestionMark = 'question-mark',
+  Publish = 'publish',
+  Profile = 'profile',
+  Plus = 'plus',
+  Pdf = 'pdf',
+  Papers = 'papers',
+  Padlock = 'padlock',
+  PadlockUnlock = 'padlock-unlock',
+  Orchid = 'orchid',
+  MyProjects = 'my-projects',
+  Minus = 'minus',
+  Meetings = 'meetings',
+  Materials = 'materials',
+  List = 'list',
+  Linkedin = 'linkedin',
+  Institutions = 'institutions',
+  Information = 'information',
+  Image = 'image',
+  Hosting = 'hosting',
+  Home = 'home',
+  Group = 'group',
+  Github = 'github',
+  Folder = 'folder',
+  Filter = 'filter',
+  Facebook = 'facebook',
+  EyeView = 'eye-view',
+  EyeHidden = 'eye-hidden',
+  Email = 'email',
+  Download = 'download',
+  DoubleArrowLeft = 'double-arrow-left',
+  Dots = 'dots',
+  Donate = 'donate',
+  Doc = 'doc',
+  Design = 'design',
+  Data = 'data',
+  Copy = 'copy',
+  Collections = 'collections',
+  Collect = 'collect',
+  Code = 'code',
+  CalendarSilhouette = 'calendar-silhouette',
+  ArrowDown = 'arrow-down',
+}
+
+export const ICONS_CODEPOINTS: Record<Icons, string> = {
+  [Icons.X]: '61697',
+  [Icons.Website]: '61698',
+  [Icons.Warning]: '61699',
+  [Icons.Upload]: '61700',
+  [Icons.Trash]: '61701',
+  [Icons.Support]: '61702',
+  [Icons.Supplements]: '61703',
+  [Icons.Sort3]: '61704',
+  [Icons.Sort2]: '61705',
+  [Icons.Sort1]: '61706',
+  [Icons.Settings]: '61707',
+  [Icons.Search]: '61708',
+  [Icons.Quotes]: '61709',
+  [Icons.QuestionMark]: '61710',
+  [Icons.Publish]: '61711',
+  [Icons.Profile]: '61712',
+  [Icons.Plus]: '61713',
+  [Icons.Pdf]: '61714',
+  [Icons.Papers]: '61715',
+  [Icons.Padlock]: '61716',
+  [Icons.PadlockUnlock]: '61717',
+  [Icons.Orchid]: '61718',
+  [Icons.MyProjects]: '61719',
+  [Icons.Minus]: '61720',
+  [Icons.Meetings]: '61721',
+  [Icons.Materials]: '61722',
+  [Icons.List]: '61723',
+  [Icons.Linkedin]: '61724',
+  [Icons.Institutions]: '61725',
+  [Icons.Information]: '61726',
+  [Icons.Image]: '61727',
+  [Icons.Hosting]: '61728',
+  [Icons.Home]: '61729',
+  [Icons.Group]: '61730',
+  [Icons.Github]: '61731',
+  [Icons.Folder]: '61732',
+  [Icons.Filter]: '61733',
+  [Icons.Facebook]: '61734',
+  [Icons.EyeView]: '61735',
+  [Icons.EyeHidden]: '61736',
+  [Icons.Email]: '61737',
+  [Icons.Download]: '61738',
+  [Icons.DoubleArrowLeft]: '61739',
+  [Icons.Dots]: '61740',
+  [Icons.Donate]: '61741',
+  [Icons.Doc]: '61742',
+  [Icons.Design]: '61743',
+  [Icons.Data]: '61744',
+  [Icons.Copy]: '61745',
+  [Icons.Collections]: '61746',
+  [Icons.Collect]: '61747',
+  [Icons.Code]: '61748',
+  [Icons.CalendarSilhouette]: '61749',
+  [Icons.ArrowDown]: '61750',
+};
