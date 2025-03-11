ed3a6b0e2134de6365c58182c3770ccffaecf380
Roman Nastyuk <rnastyuk@exoft.net>
2025-03-11 14:07:55 +0200

    Feat/49 - Design sign-up form (#15)
    * feat(sign-up-design): fixed styles, added dynamic header button label rendering

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
 import { Button } from 'primeng/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -10,4 +17,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  #router = inject(Router);
+
+  #currentUrl = toSignal(this.#router.events.pipe(map(() => this.#router.url)));
+
+  protected readonly authButtonText = computed(() =>
+    this.#currentUrl()?.includes('sign-up') ? 'Sign In' : 'Sign Up',
+  );
+
+  protected readonly authButtonLink = computed(() =>
+    this.#currentUrl()?.includes('sign-up') ? '/login' : '/sign-up',
+  );
+}
