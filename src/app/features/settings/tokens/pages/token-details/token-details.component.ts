8bc4f64ec644b5401faffc3ab6e766e092032a79
nsemets <nsemets@exoft.net>
2025-06-23 11:31:54 +0300

    fix(tokens): added loading state to tokens details

--- src/app/features/settings/tokens/pages/token-details/token-details.component.ts
+++ src/app/features/settings/tokens/pages/token-details/token-details.component.ts
@@ -1,4 +1,4 @@
-import { createDispatchMap, Store } from '@ngxs/store';
+import { createDispatchMap, select, Store } from '@ngxs/store';
 
 import { TranslatePipe } from '@ngx-translate/core';
 
@@ -7,10 +7,9 @@ import { Card } from 'primeng/card';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
-import { IconComponent } from '@osf/shared/components';
+import { IconComponent, LoadingSpinnerComponent } from '@osf/shared/components';
 import { CustomConfirmationService } from '@osf/shared/services';
 
 import { TokenAddEditFormComponent } from '../../components';
@@ -18,7 +17,7 @@ import { DeleteToken, GetTokenById, TokensSelectors } from '../../store';
 
 @Component({
   selector: 'osf-token-details',
-  imports: [Button, Card, FormsModule, RouterLink, TokenAddEditFormComponent, TranslatePipe, IconComponent],
+  imports: [Button, Card, RouterLink, TranslatePipe, TokenAddEditFormComponent, IconComponent, LoadingSpinnerComponent],
   providers: [DialogService, DynamicDialogRef],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './token-details.component.html',
@@ -34,6 +33,7 @@ export class TokenDetailsComponent implements OnInit {
 
   tokenId = signal(this.route.snapshot.paramMap.get('id') ?? '');
   token = computed(() => this.store.selectSignal(TokensSelectors.getTokenById)()(this.tokenId()));
+  isLoading = select(TokensSelectors.isTokensLoading);
 
   ngOnInit(): void {
     if (this.tokenId()) {
