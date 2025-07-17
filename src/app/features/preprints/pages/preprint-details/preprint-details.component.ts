895ee7667bd22e5064548773f100d05be59fde53
Roma <nikitinroma2605@gmail.com>
2025-07-17 11:52:20 +0300

    feat(preprint-details): Implemented basic logic&layout for details page

---
+++ src/app/features/preprints/pages/preprint-details/preprint-details.component.ts
@@ -0,0 +1,68 @@
+import { createDispatchMap, select, Store } from '@ngxs/store';
+
+import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { Skeleton } from 'primeng/skeleton';
+
+import { map, of } from 'rxjs';
+
+import { ChangeDetectionStrategy, Component, HostBinding, inject, OnDestroy, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { PreprintFileSectionComponent } from '@osf/features/preprints/components/preprint-details/preprint-file-section/preprint-file-section.component';
+import { FetchPreprintById, PreprintSelectors } from '@osf/features/preprints/store/preprint';
+import { GetPreprintProviderById, PreprintProvidersSelectors } from '@osf/features/preprints/store/preprint-providers';
+import { CreateNewVersion, PreprintStepperSelectors, ResetState } from '@osf/features/preprints/store/preprint-stepper';
+
+@Component({
+  selector: 'osf-preprint-details',
+  imports: [Skeleton, PreprintFileSectionComponent, Card, Button],
+  templateUrl: './preprint-details.component.html',
+  styleUrl: './preprint-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PreprintDetailsComponent implements OnInit, OnDestroy {
+  @HostBinding('class') classes = 'flex-1 flex flex-column w-full';
+
+  private readonly route = inject(ActivatedRoute);
+  private readonly store = inject(Store);
+  private readonly router = inject(Router);
+
+  private providerId = toSignal(this.route.params.pipe(map((params) => params['providerId'])) ?? of(undefined));
+  private preprintId = toSignal(this.route.params.pipe(map((params) => params['preprintId'])) ?? of(undefined));
+
+  private actions = createDispatchMap({
+    getPreprintProviderById: GetPreprintProviderById,
+    resetState: ResetState,
+    fetchPreprintById: FetchPreprintById,
+    createNewVersion: CreateNewVersion,
+  });
+
+  preprintProvider = select(PreprintProvidersSelectors.getPreprintProviderDetails(this.providerId()));
+  isPreprintProviderLoading = select(PreprintProvidersSelectors.isPreprintProviderDetailsLoading);
+  preprint = select(PreprintSelectors.getPreprint);
+  isPreprintLoading = select(PreprintSelectors.isPreprintLoading);
+
+  ngOnInit() {
+    this.actions.fetchPreprintById(this.preprintId());
+    this.actions.getPreprintProviderById(this.providerId());
+  }
+
+  ngOnDestroy() {
+    this.actions.resetState();
+  }
+
+  editPreprintClicked() {
+    this.router.navigate(['preprints', this.providerId(), 'edit', this.preprintId()]);
+  }
+
+  createNewVersionClicked() {
+    this.actions.createNewVersion(this.preprintId()).subscribe({
+      complete: () => {
+        const newVersionPreprint = this.store.selectSnapshot(PreprintStepperSelectors.getPreprint);
+        this.router.navigate(['preprints', this.providerId(), 'new-version', newVersionPreprint!.id]);
+      },
+    });
+  }
+}
