41f60a25464ba644a50a44ef48a00427c01ea8e8
nsemets <nsemets@exoft.net>
2025-07-22 17:50:17 +0300

    fix(profile-settings): updated education tab

--- src/app/features/settings/profile-settings/components/education/education.component.ts
+++ src/app/features/settings/profile-settings/components/education/education.component.ts
@@ -3,24 +3,30 @@ import { createDispatchMap, select } from '@ngxs/store';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { Button } from 'primeng/button';
-import { Checkbox } from 'primeng/checkbox';
-import { DatePicker } from 'primeng/datepicker';
-import { InputText } from 'primeng/inputtext';
 
-import { ChangeDetectionStrategy, Component, effect, HostBinding, inject } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  effect,
+  HostBinding,
+  inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { UpdateProfileSettingsEducation, UserSelectors } from '@osf/core/store/user';
 import { Education } from '@osf/shared/models';
-import { LoaderService, ToastService } from '@osf/shared/services';
-import { CustomValidators } from '@osf/shared/utils';
+import { CustomConfirmationService, LoaderService, ToastService } from '@osf/shared/services';
+import { CustomValidators, findChangedFields } from '@osf/shared/utils';
 
-import { MAX_DATE, MIN_DATE } from '../../constants';
 import { EducationForm } from '../../models';
+import { EducationFormComponent } from '../education-form/education-form.component';
 
 @Component({
   selector: 'osf-education',
-  imports: [ReactiveFormsModule, Button, InputText, DatePicker, Checkbox, TranslatePipe],
+  imports: [ReactiveFormsModule, Button, TranslatePipe, EducationFormComponent],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,47 +34,23 @@ import { EducationForm } from '../../models';
 export class EducationComponent {
   @HostBinding('class') classes = 'flex flex-column gap-5';
 
-  maxDate = MAX_DATE;
-  minDate = MIN_DATE;
-
-  readonly fb = inject(FormBuilder);
-  protected readonly educationForm = this.fb.group({ educations: this.fb.array<EducationForm>([]) });
-
   private readonly loaderService = inject(LoaderService);
+  private readonly customConfirmationService = inject(CustomConfirmationService);
   private readonly toastService = inject(ToastService);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly cd = inject(ChangeDetectorRef);
+  private readonly fb = inject(FormBuilder);
 
+  readonly educationForm = this.fb.group({ educations: this.fb.array<FormGroup>([]) });
   readonly actions = createDispatchMap({ updateProfileSettingsEducation: UpdateProfileSettingsEducation });
   readonly educationItems = select(UserSelectors.getEducation);
 
   constructor() {
-    effect(() => {
-      const educations = this.educationItems();
-
-      if (educations && educations.length > 0) {
-        this.educations.clear();
-
-        educations.forEach((education) => {
-          const newEducation = this.fb.group({
-            institution: [education.institution, CustomValidators.requiredTrimmed()],
-            department: [education.department],
-            degree: [education.degree],
-            startDate: [new Date(+education.startYear, education.startMonth - 1)],
-            endDate: education.ongoing
-              ? ''
-              : education.endYear && education.endMonth
-                ? [new Date(+education.endYear, education.endMonth - 1)]
-                : null,
-            ongoing: [education.ongoing],
-          });
-
-          this.educations.push(newEducation);
-        });
-      }
-    });
+    effect(() => this.setInitialData());
   }
 
-  get educations() {
-    return this.educationForm.get('educations') as FormArray;
+  get educations(): FormArray<FormGroup> {
+    return this.educationForm.get('educations') as FormArray<FormGroup>;
   }
 
   removeEducation(index: number): void {
@@ -76,55 +58,112 @@ export class EducationComponent {
   }
 
   addEducation(): void {
-    const newEducation = this.fb.group({
-      institution: ['', [CustomValidators.requiredTrimmed()]],
-      department: [''],
-      degree: [''],
-      startDate: [null],
-      endDate: [null],
-      ongoing: [false],
+    if (this.educationForm.invalid) {
+      this.educationForm.markAllAsTouched();
+      return;
+    }
+
+    this.educations.push(this.createEducationFormGroup());
+  }
+
+  discardChanges(): void {
+    if (!this.hasFormChanges()) {
+      return;
+    }
+
+    this.customConfirmationService.confirmDelete({
+      headerKey: 'common.discardChangesDialog.header',
+      messageKey: 'common.discardChangesDialog.message',
+      onConfirm: () => {
+        this.setInitialData();
+        this.cd.markForCheck();
+      },
     });
-    this.educations.push(newEducation);
   }
 
   saveEducation(): void {
-    const educations = this.educations.value as EducationForm[];
+    if (this.educationForm.invalid) {
+      this.educationForm.markAllAsTouched();
+      return;
+    }
 
-    const formattedEducation = educations.map((education) => ({
-      institution: education.institution,
-      department: education.department,
-      degree: education.degree,
-      startYear: this.setupDates(education.startDate, null).startYear,
-      startMonth: this.setupDates(education.startDate, null).startMonth,
-      endYear: education.ongoing ? null : this.setupDates('', education.endDate).endYear,
-      endMonth: education.ongoing ? null : this.setupDates('', education.endDate).endMonth,
-      ongoing: education.ongoing,
-    })) satisfies Education[];
-
+    const formattedEducation = this.educations.value.map((education) => this.mapFormToEducation(education));
     this.loaderService.show();
 
-    this.actions.updateProfileSettingsEducation({ education: formattedEducation }).subscribe(() => {
-      this.loaderService.hide();
-      this.toastService.showSuccess('settings.profileSettings.education.successUpdate');
+    this.actions
+      .updateProfileSettingsEducation({ education: formattedEducation })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.loaderService.hide();
+          this.toastService.showSuccess('settings.profileSettings.education.successUpdate');
+        },
+        error: () => this.loaderService.hide(),
+      });
+  }
+
+  private hasFormChanges(): boolean {
+    if (this.educations.length !== this.educationItems().length) {
+      return true;
+    }
+
+    return this.educations.value.some((formEducation, index) => {
+      const initialEdu = this.educationItems()[index];
+      if (!initialEdu) return true;
+
+      const formattedFormEducation = this.mapFormToEducation(formEducation);
+      const changedFields = findChangedFields<Education>(formattedFormEducation, initialEdu);
+
+      return Object.keys(changedFields).length > 0;
     });
   }
 
-  private setupDates(
-    startDate: Date | string,
-    endDate: Date | null
-  ): {
-    startYear: number;
-    startMonth: number;
-    endYear: number | null;
-    endMonth: number | null;
-  } {
-    const start = new Date(startDate);
-    const end = endDate ? new Date(endDate) : null;
+  private createEducationFormGroup(education?: Partial<EducationForm>): FormGroup {
+    return this.fb.group({
+      institution: [education?.institution ?? '', CustomValidators.requiredTrimmed()],
+      department: [education?.department ?? ''],
+      degree: [education?.degree ?? ''],
+      startDate: [education?.startDate ?? null],
+      endDate: [education?.endDate ?? null],
+      ongoing: [education?.ongoing ?? false],
+    });
+  }
+
+  private setInitialData(): void {
+    const educations = this.educationItems();
+    if (!educations?.length) return;
+
+    this.educations.clear();
+    educations
+      .map((education) => this.mapEducationToForm(education))
+      .forEach((education) => this.educations.push(this.createEducationFormGroup(education)));
+  }
+
+  private mapFormToEducation(education: EducationForm): Education {
     return {
-      startYear: start.getFullYear(),
-      startMonth: start.getMonth() + 1,
-      endYear: end ? end.getFullYear() : null,
-      endMonth: end ? end.getMonth() + 1 : null,
+      institution: education.institution,
+      department: education.department,
+      degree: education.degree,
+      startYear: education.startDate?.getFullYear() ?? new Date().getFullYear(),
+      startMonth: (education.startDate?.getMonth() ?? 0) + 1,
+      endYear: education.ongoing ? null : (education.endDate?.getFullYear() ?? null),
+      endMonth: education.ongoing ? null : education.endDate ? education.endDate.getMonth() + 1 : null,
+      ongoing: education.ongoing,
+    };
+  }
+
+  private mapEducationToForm(education: Education): EducationForm {
+    return {
+      institution: education.institution,
+      department: education.department,
+      degree: education.degree,
+      startDate: new Date(+education.startYear, education.startMonth - 1),
+      endDate: education.ongoing
+        ? null
+        : education.endYear && education.endMonth
+          ? new Date(+education.endYear, education.endMonth - 1)
+          : null,
+      ongoing: education.ongoing,
     };
   }
 }
