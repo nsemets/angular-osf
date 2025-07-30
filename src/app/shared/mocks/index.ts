a60658d984ddb325fbfaa3cdbc0e8e44f76b37a2
Diana <nalivaykodiana@gmail.com>
2025-07-30 16:38:25 +0300

    test(refactor): move meeting mock data to separate file

--- src/app/shared/mocks/index.ts
+++ src/app/shared/mocks/index.ts
@@ -1,5 +1,5 @@
 export { MOCK_USER } from './data.mock';
-export { MOCK_MEETING } from './data.mock';
-export { MOCK_MEETING_SUBMISSIONS } from './data.mock';
+export { MOCK_MEETING } from './meeting.mock';
+export { MOCK_MEETING_SUBMISSIONS } from './meeting.mock';
 export { MOCK_STORE } from './mock-store.mock';
 export { TranslateServiceMock } from './translate.service.mock';
