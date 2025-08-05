7aee1e549bd82bb9e6317776240b30914d2b46cd
Diana <nalivaykodiana@gmail.com>
2025-08-05 11:04:27 +0300

    test(testing): added tests for account-settings component

--- src/app/features/settings/account-settings/account-settings.component.spec.ts
+++ src/app/features/settings/account-settings/account-settings.component.spec.ts
@@ -1,25 +1,86 @@
-import { provideStore } from '@ngxs/store';
+import { Store } from '@ngxs/store';
 
-import { MockComponent } from 'ng-mocks';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MockComponents, MockPipe, MockProvider } from 'ng-mocks';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
-import { UserState } from '@osf/core/store/user';
+import { UserSelectors } from '@osf/core/store/user';
+import {
+  AffiliatedInstitutionsComponent,
+  ChangePasswordComponent,
+  ConnectedEmailsComponent,
+  ConnectedIdentitiesComponent,
+  DeactivateAccountComponent,
+  DefaultStorageLocationComponent,
+  ShareIndexingComponent,
+  TwoFactorAuthComponent,
+} from '@osf/features/settings/account-settings/components';
+import { AccountSettingsSelectors } from '@osf/features/settings/account-settings/store';
 import { SubHeaderComponent } from '@osf/shared/components';
+import { MOCK_STORE, MOCK_USER, MockCustomConfirmationServiceProvider, TranslateServiceMock } from '@shared/mocks';
+import { ToastService } from '@shared/services';
 
 import { AccountSettingsComponent } from './account-settings.component';
 
 describe('AccountSettingsComponent', () => {
   let component: AccountSettingsComponent;
   let fixture: ComponentFixture<AccountSettingsComponent>;
+  const store = MOCK_STORE;
 
   beforeEach(async () => {
+    store.selectSignal.mockImplementation((selector) => {
+      switch (selector) {
+        case UserSelectors.getCurrentUser:
+          return () => MOCK_USER;
+
+        case AccountSettingsSelectors.getEmails:
+          return () => [];
+
+        case AccountSettingsSelectors.getAccountSettings:
+          return () => null;
+
+        case AccountSettingsSelectors.getExternalIdentities:
+          return () => null;
+
+        case AccountSettingsSelectors.getRegions:
+          return () => null;
+
+        case AccountSettingsSelectors.getUserInstitutions:
+          return () => null;
+
+        default:
+          return () => [];
+      }
+    });
     await TestBed.configureTestingModule({
-      imports: [AccountSettingsComponent, MockComponent(SubHeaderComponent)],
-      providers: [provideNoopAnimations(), provideHttpClient(), provideHttpClientTesting(), provideStore([UserState])],
+      imports: [
+        AccountSettingsComponent,
+        ...MockComponents(
+          SubHeaderComponent,
+          ConnectedEmailsComponent,
+          DefaultStorageLocationComponent,
+          ConnectedIdentitiesComponent,
+          ShareIndexingComponent,
+          ChangePasswordComponent,
+          TwoFactorAuthComponent,
+          DeactivateAccountComponent,
+          AffiliatedInstitutionsComponent
+        ),
+        MockPipe(TranslatePipe),
+      ],
+      providers: [
+        MockCustomConfirmationServiceProvider,
+        TranslateServiceMock,
+        MockProvider(ToastService),
+        provideNoopAnimations(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        MockProvider(Store, store),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccountSettingsComponent);
@@ -29,5 +90,15 @@ describe('AccountSettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not dispatch actions when currentUser is null', () => {
+    store.selectSignal.mockImplementation((selector) =>
+      selector === UserSelectors.getCurrentUser ? () => null : () => []
+    );
+
+    store.dispatch.mockClear();
+
+    expect(store.dispatch).not.toHaveBeenCalled();
   });
 });
