87c4bc5498637472583955c554e0e466ef768c09
NazarMykhalkevych <nmykhalkevych@exoft.net>
2025-08-09 12:49:35 +0300

    feat(registration): fixed  make decision

--- src/app/shared/enums/trigger-action.enum.ts
+++ src/app/shared/enums/trigger-action.enum.ts
@@ -1,5 +1,18 @@
-export enum TriggerAction {
-  Accept = 'accept',
-  Reject = 'reject',
-  ForceWithdraw = 'force_withdraw',
+export enum ReviewActionTrigger {
+  Submit = 'submit', // registration submitted by admins
+  AcceptSubmission = 'accept_submission', // accept submission
+  RejectSubmission = 'reject_submission', // reject submission
+  ForceWithdraw = 'force_withdraw', // force withdraw without request
+  RequestWithdrawal = 'request_withdrawal', // request to withdraw by contributors
+  AcceptWithdrawal = 'accept_withdrawal', // accept withdrawal request
+  RejectWithdrawal = 'reject_withdrawal', // deny withdrawal request
+  RequestEmbargoTermination = 'request_embargo_termination', // admin requests embargo termination
+}
+
+export enum SchemaResponseActionTrigger {
+  SubmitRevision = 'submit',
+  AdminApproveRevision = 'approve',
+  AdminRejectRevision = 'admin_reject',
+  AcceptRevision = 'accept',
+  RejectRevision = 'moderator_reject',
 }
