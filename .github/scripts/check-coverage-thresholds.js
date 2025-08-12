76a5f2c2e53811f7932d79e1cfe499060752a430
Brian Pilati <brianpilati@cos.io>
2025-08-12 13:52:31 -0500

    feat(warns): Updated the pipeline locally to be a warn

--- .github/scripts/check-coverage-thresholds.js
+++ .github/scripts/check-coverage-thresholds.js
@@ -8,24 +8,55 @@ const thresholds = jestConfig.coverageThreshold.global;
 
 let failed = false;
 const errors = [];
+const leftBracket = `*${' '.repeat(3)}`;
+const rightBracket = `${' '.repeat(3)}*`;
+const warnMessage =
+  'This is only a warn. In the future, you will not be able to push to github until the thresholds are updated.';
+
+function formatErrorsWithAlignedStars(error, isSecondline = false) {
+  const totalWidth = warnMessage.length;
+  const spaces = totalWidth - error.length - (isSecondline ? 4 : 0);
+  return `${leftBracket}${isSecondline ? '\t' : ''}${error}${' '.repeat(spaces)}${rightBracket}`;
+}
+
+// Example usage:
+
 for (const key of ['branches', 'functions', 'lines', 'statements']) {
   const current = summary[key].pct;
   const threshold = thresholds[key];
   if (current > threshold) {
     errors.push(
-      `Coverage for ${key} (${current}%) is above the threshold (${threshold}%).\n\tPlease update the coverageThreshold.global.${key} in the jest.config.js to ${current}!`
+      formatErrorsWithAlignedStars(`Coverage for ${key} (${current}%) is above the threshold (${threshold}%).`)
     );
+    errors.push(
+      formatErrorsWithAlignedStars(
+        `Please update the coverageThreshold.global.${key} in the jest.config.js to ---> ${current} <---`,
+        true
+      )
+    );
+    errors.push(`${leftBracket}${' '.repeat(warnMessage.length)}${rightBracket}`);
     failed = true;
   }
 }
 
 if (failed) {
+  const stars = '*'.repeat(warnMessage.length + 8);
   execSync('clear', { stdio: 'inherit' });
+  console.log(warnMessage.length);
   console.log('\n\nCongratulations! You have successfully run the coverage check and added tests.');
   console.log('\n\nThe jest.config.js file is not insync with your new test additions.');
   console.log('Please update the coverage thresholds in jest.config.js.');
   console.log('You will need to commit again once you have updated the jst.config.js file.');
-  console.log('This is only necessary until we hit 100% coverage.\n\n');
-  errors.forEach((err) => console.error(`${err}\n`));
-  process.exit(1);
+  console.log('This is only necessary until we hit 100% coverage.');
+  console.log(`\n\n${stars}`);
+  errors.forEach((err) => {
+    console.error(err);
+  });
+  console.log(`${stars}`);
+  console.log(`\n\n${stars}`);
+  console.log(`${leftBracket}${' '.repeat(warnMessage.length)}${rightBracket}`);
+  console.log(`${leftBracket}${warnMessage}${rightBracket}`);
+  console.log(`${leftBracket}${' '.repeat(warnMessage.length)}${rightBracket}`);
+  console.log(`${stars}\n\n`);
+  // process.exit(1);
 }
