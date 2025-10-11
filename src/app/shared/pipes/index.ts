a9fab736a412d7f441acad6f9d33bf060ae02ced
nmykhalkevych-exoft <nmykhalkevych@exoft.net>
2025-10-11 22:00:40 +0300

    Fix/prod hot fixes (#642)
    * fix(prod-bugs): wiki and hide copy file action
    
    * fix(wiki): pagination
    
    * fix(pipe): fixed special char

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -2,6 +2,7 @@ export { CitationFormatPipe } from './citation-format.pipe';
 export { DateAgoPipe } from './date-ago.pipe';
 export { DecodeHtmlPipe } from './decode-html.pipe';
 export { FileSizePipe } from './file-size.pipe';
+export { FixSpecialCharPipe } from './fix-special-char.pipe';
 export { InterpolatePipe } from './interpolate.pipe';
 export { MonthYearPipe } from './month-year.pipe';
 export { SortByDatePipe } from './sort-by-date.pipe';
