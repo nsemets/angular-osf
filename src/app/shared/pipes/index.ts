f54d5bd22bb23c9832a988bdeb5ae5f1901fd82a
nsemets <nsemets@exoft.net>
2025-10-15 18:12:06 +0300

    fix(preregistration): fixed breaks in registration (#665)

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -5,5 +5,7 @@ export { FileSizePipe } from './file-size.pipe';
 export { FixSpecialCharPipe } from './fix-special-char.pipe';
 export { InterpolatePipe } from './interpolate.pipe';
 export { MonthYearPipe } from './month-year.pipe';
+export { SafeHtmlPipe } from './safe-html.pipe';
+export { SafeUrlPipe } from './safe-url.pipe';
 export { SortByDatePipe } from './sort-by-date.pipe';
 export { WrapFnPipe } from './wrap-fn.pipe';
