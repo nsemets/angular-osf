4c512b44f931a3d12fbcc6a716e619f5ea650ef8
nsemets <nsemets@exoft.net>
2025-10-23 19:06:54 +0300

    fix(moderators): fixed moderators rights

--- src/app/features/moderation/models/preprint-provider-moderation-info.model.ts
+++ src/app/features/moderation/models/preprint-provider-moderation-info.model.ts
@@ -1,9 +1,12 @@
+import { ReviewPermissions } from '@osf/shared/enums';
+
 export interface PreprintProviderModerationInfo {
   id: string;
   name: string;
-  submissionCount?: number;
+  permissions: ReviewPermissions[];
   reviewsCommentsAnonymous: boolean;
   reviewsCommentsPrivate: boolean;
   reviewsWorkflow: string;
+  submissionCount?: number;
   supportEmail: string | null;
 }
