ce48065fb197fc47670821d487bddb815b95ab5b
nsemets <nsemets@exoft.net>
2026-04-14 17:20:05 +0300

    [ENG-10553] ImpactStory Profile Icon Is Broken (#944)
    - Ticket: https://openscience.atlassian.net/browse/ENG-10553
    - Feature flag: n/a
    
    ## Summary of Changes
    1. Fixed impact story icon.

--- src/app/shared/constants/social-links.const.ts
+++ src/app/shared/constants/social-links.const.ts
@@ -39,7 +39,7 @@ export const SOCIAL_LINKS: SocialLinksModel[] = [
     address: 'https://impactstory.org/u/',
     placeholder: 'profileID',
     key: 'impactStory',
-    icon: 'impactStory.png',
+    icon: 'impactstory.png',
   },
   {
     id: 6,
