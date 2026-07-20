6e4d7f1249ad8627811aec1d79484a6855f89a61
nsemets <nsemets@exoft.net>
2026-07-20 18:54:51 +0300

    fix(feature-flags): removed unused flag

--- src/app/shared/constants/feature-flags.const.ts
+++ src/app/shared/constants/feature-flags.const.ts
@@ -1,5 +1,4 @@
 export const FEATURE_FLAGS = {
-  COLLECTION_SUBMISSION_WITH_CEDAR: 'collection_submission_with_cedar',
   WORKFLOW_LAUNCHER: 'workflow_launcher',
   GRAVY_REDIRECT: 'gravy_redirect',
 } as const;
