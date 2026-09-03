199722f42a876a21bcc1e15c04bfce2373c4846d
nsemets <nsemets@exoft.net>
2026-09-03 12:12:37 +0300

    feat(view-only-link): removed button

--- src/app/shared/components/view-only-link-message/view-only-link-message.component.ts
+++ src/app/shared/components/view-only-link-message/view-only-link-message.component.ts
@@ -1,33 +1,14 @@
 import { TranslatePipe } from '@ngx-translate/core';
 
-import { Button } from 'primeng/button';
 import { Message } from 'primeng/message';
 
-import { isPlatformBrowser } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'osf-view-only-link-message',
-  imports: [Message, TranslatePipe, Button],
+  imports: [Message, TranslatePipe],
   templateUrl: './view-only-link-message.component.html',
   styleUrl: './view-only-link-message.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ViewOnlyLinkMessageComponent {
-  private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
-  private readonly router = inject(Router);
-
-  handleLeaveViewOnlyView(): void {
-    if (!this.isBrowser) {
-      return;
-    }
-
-    this.router
-      .navigate([], {
-        queryParams: { view_only: null },
-        queryParamsHandling: 'merge',
-      })
-      .then(() => window.location.reload());
-  }
-}
+export class ViewOnlyLinkMessageComponent {}
